e0dc06993195fe9ccccd5cfe44d00e84a864ee08
Huba <hieu.dcu@gmail.com>
2023-12-19 17:21:21 +1300

    Added webpacks

---
+++ DOMStuff.js
@@ -0,0 +1,7 @@
+function addProject() {
+    const clickAddProject = document.getElementById("addProjectBtn");
+    const toMyProject = document.getElementById("addProj").value;
+    clickAddProject.addEventListener("click", event => {
+        toMyProject.textContent = 
+    })
+}
--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+import { compareAsc, format } from './node_modules/date-fns';
+import * as DOM from './DOMStuff';
+
 class Project {
-    constructor(name, type, requirement) {
-        this.name = name;
-        this.type = type;
-        this.requirement = requirement;
+    constructor (projectName) {
+        this.projectName = projectName;
     }
-    
 }
+
+
+class Task {
+    constructor (taskName, taskDesc, taskPriority, taskDue) {
+        this.taskName = taskName;
+        this.taskDesc = taskDesc;
+        this.taskPriority = taskPriority;
+        this.taskDue = taskDue;
+    }
+}
+
+DOM.addProject();
